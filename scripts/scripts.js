c17749a782ea730c3a4914bab6a6885108309ea0
MarkCortejos <markcortejos@gmail.com>
2019-03-19 12:39:09 -0400

    semi-working drop menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,4 +5,9 @@ $(function() {
     speed: 1000
   });
 
+  $('.hamburger > i').click(function() {
+    $('.hamburger > i').toggleClass('fa-times fa-bars');
+    $('.dropMenu').slideToggle();
+  })
+
 }) // Doc Ready Ends
