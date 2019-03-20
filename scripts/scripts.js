47c5efe99dec8cfe35fd82df0128a595bb99387b
MarkCortejos <markcortejos@gmail.com>
2019-03-19 21:24:13 -0400

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,24 @@
 // Doc Ready Begins
 $(function() {
 
+  // Smooth scroll
   $('a').smoothScroll({
     speed: 1000
   });
 
-  $('.hamburger > i').click(function() {
+  // Opens and closes hamburger menu on click
+  $('.hamburger > i').on('click', function() {
     $('.hamburger > i').toggleClass('fa-times fa-bars');
     $('.dropMenu').slideToggle();
   })
+  
+  //Closes hamburger when a nav link is clicked
+  $('.dropMenu a').on('click', function() {
+    $('.hamburger > i').toggleClass('fa-times fa-bars');
+    $('.dropMenu').slideToggle();
+  });
 
+  // prevent refresh on form submit
   $('#contact').on('submit', function(event) {
     event.preventDefault();
   })
